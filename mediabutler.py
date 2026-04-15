10eecf19f3b967d24c70fa396a0a9b79665a122b
allonthefarm <allonthefarm@gmail.com>
2026-04-15 15:34:12 -0400

    Added a function to add original file name ti metadata

--- mediabutler.py
+++ mediabutler.py
@@ -1,5 +1,6 @@
 from config import DATE_PATTERNS
 from mutagen import File
+from pathlib import Path
 import re
 def normalize_date(raw):
     rawstr = str(raw)
@@ -31,3 +32,21 @@ def extract_date_from_metadata(filepath):
     if raw is None:
         return None
     return normalize_date(raw)
+
+def original_filename_to_metadata(filepath, filename):
+    media = File(filepath)
+    if media is None:
+        return None
+    if Path(filepath).suffix == ".mp4":
+        key = "----:com.apple.iTunes:MEDIABUTLER_ORIGINAL"
+    else: key = "MEDIABUTLER_ORIGINAL"  
+    if media.tags is None:
+        media.add_tags()
+    tags = media.tags.get(key)
+    if tags is not None:
+        return None
+    if Path(filepath).suffix == ".mp4":
+        media.tags[key] = filename.encode("utf-8")
+    else: 
+        media.tags[key] = filename 
+    media.save()
